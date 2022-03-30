cb57ffcfc01b5eef1c70b9a6208d3686f649fe50
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-03-30 16:25:54 +0200

    Updating frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 // import logo from './logo.svg';
 // import './App.css';
-
-function App() {
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+const App = () => {
   return (
     <>
-    <h1>Welcome to Proshop</h1>
+    <Header />
+      <main>
+        <Container>
+          <HomeScreen />
+        </Container>
+    </main>
+    <Footer />
     </>
   );
-}
+};
 
 export default App;
